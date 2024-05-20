42584f5819887e1ee0980558af00290022f491dd
Wilson Xavier <will.xavier01@gmail.com>
2024-05-20 17:57:48 -0300

    Tela de Login

---
+++ src/pages/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../styles/Login.css';
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src='./assets/logo-cafe.png' alt="Logo" />
+      </div>
+      <div className="login-box">
+        <form action="/login" method="post">
+          <div className="input-group">
+            <label htmlFor="cpf">Usuário</label>
+            <input type="text" id="cpf" name="cpf" placeholder="Digite seu CPF" maxLength="11" pattern="\d{11}" inputMode="numeric" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" name="password" placeholder="Digite sua senha" maxLength="6" pattern="\d{6}" inputMode="numeric" required />
+          </div>
+          <div className="forgot-password">
+            <a href=" ">Esqueci minha senha</a>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <div className="register">
+          <a href=" ">Novo usuário? <strong>Registro</strong></a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
